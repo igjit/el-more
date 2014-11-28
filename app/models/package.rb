069dd8419b729b39e3474784fb68e5a6b2c0b18c
igjit <igjit1@gmail.com>
2014-11-28 19:30:30 +0900

    Implement Package#similar_packages

--- app/models/package.rb
+++ app/models/package.rb
@@ -1,8 +1,23 @@
 class Package < ActiveRecord::Base
+  include Redis::Objects
+
   belongs_to :cask
   has_many :cask_packages
   has_many :casks, through: :cask_packages
 
   validates :name, presence: true
   validates :repo_type, presence: true
+
+  sorted_set :similarity
+
+  def similar_packages(limit: nil)
+    end_index = limit.nil? ? -1 : limit - 1
+
+    id_scores = similarity.revrange(0, end_index, withscores: true)
+    ids = id_scores.map { |a, b| a.to_i }
+    scores = id_scores.map(&:last)
+    packages = Package.find(ids).index_by(&:id).slice(*ids).values
+
+    packages.zip(scores)
+  end
 end
--- spec/models/package_spec.rb
+++ spec/models/package_spec.rb
@@ -4,4 +4,24 @@ RSpec.describe Package, :type => :model do
   it { should have_many(:casks).through(:cask_packages) }
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:repo_type) }
+
+  describe '#similar_packages' do
+    before :each do
+      Redis.current.flushdb
+
+      @package_a = create(:package)
+      @package_b = create(:package)
+      @package_c = create(:package)
+      @package_d = create(:package)
+
+      @package_a.similarity[@package_b.id] = 0.4
+      @package_a.similarity[@package_c.id] = 0.9
+      @package_a.similarity[@package_d.id] = 0.8
+    end
+
+    it { expect(@package_a.similar_packages(limit: 1)).to eq [[@package_c, 0.9]] }
+    it { expect(@package_a.similar_packages(limit: 2)).to eq [[@package_c, 0.9], [@package_d, 0.8]] }
+    it { expect(@package_a.similar_packages(limit: 10)).to eq [[@package_c, 0.9], [@package_d, 0.8], [@package_b, 0.4]] }
+    it { expect(@package_a.similar_packages()).to eq [[@package_c, 0.9], [@package_d, 0.8], [@package_b, 0.4]] }
+  end
 end
