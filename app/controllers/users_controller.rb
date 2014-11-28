704fba30ce82651032cb24803a366ffe83fb3535
igjit <igjit1@gmail.com>
2014-11-28 23:41:45 +0900

    Implement users#index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
+  PER = 10
+
   def index
+    @users = User.page(params[:page]).per(PER).using_cask_for_configuration.order(followers: :desc)
   end
 
   def show
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
   has_many :casks
 
   validates :login, presence: true
+
+  scope :using_cask_for_configuration, -> { where('casks.configuration' => true).joins(:casks).uniq(:id) }
 end
