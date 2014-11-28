e0ba74d7897de75b03f458722a9ace5122a47e38
igjit <igjit1@gmail.com>
2014-11-28 17:51:25 +0900

    Implement UserAttributesUpdater

---
+++ lib/user_attributes_updater.rb
@@ -0,0 +1,31 @@
+require 'open-uri'
+require 'json'
+
+module UserAttributesUpdater
+  module_function
+
+  GITHUB_API_URL = 'https://api.github.com'
+
+  def update(users, access_token)
+    users.each do |user|
+      begin
+        update_user!(user, access_token: access_token)
+      rescue => e
+        # TODO: Retry
+        warn "Update failed. #{user} #{e}"
+      end
+      sleep 10 unless access_token
+    end
+  end
+
+  def update_user!(user, access_token: nil)
+    url = "#{GITHUB_API_URL}/users/#{user.login}"
+    url << "?access_token=#{access_token}" if access_token
+    attr = JSON(open(url) { |f| f.read })
+    user.update_attributes!(name: attr['name'],
+                            url: attr['html_url'],
+                            avatar_url: attr['avatar_url'],
+                            followers: attr['followers'],
+                            following: attr['following'])
+  end
+end
