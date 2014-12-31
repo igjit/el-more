4d69f80ac339b2a8f5b094493268fd9aeca1951c
igjit <igjit1@gmail.com>
2014-12-31 21:45:05 +0900

    Setup DatabaseCleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -49,4 +49,15 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
   config.include FactoryGirl::Syntax::Methods
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
 end
