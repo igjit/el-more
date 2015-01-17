5a3868af833f8ffc5ed59a3e5b1614d882e1825f
igjit <igjit1@gmail.com>
2015-01-17 23:53:39 +0900

    Add example

--- spec/models/cask_spec.rb
+++ spec/models/cask_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe Cask, :type => :model do
   it { should have_many(:packages).through(:cask_packages) }
   it { should validate_presence_of(:url) }
   it { should validate_presence_of(:raw_url) }
+
+  describe "#path" do
+    it "returns path" do
+      cask = create(:cask, url: 'https://github.com/username/.emacs.d/blob/53da80c63c467fac27d79e8c9c8653c81d4bb0a1/Cask')
+      expect(cask.path).to eq '.emacs.d/Cask'
+    end
+  end
 end
